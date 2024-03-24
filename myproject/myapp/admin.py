8d3068146f7aad0d33eaa02035d6ae744f4b319b
saumitraid <saumitraid.das@gmail.com>
2024-03-24 16:30:18 +0530

    24-03-2024

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from . models import Student, Category, Item
 
+
+
+admin.site.site_header='My WebSite'
+admin.site.site_title='Administration'
+admin.site.index_title='My WebSite'
 # Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display=('name','email_id','city')
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=('cat_name','cat_desc')
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    def item_image(self, obj):
+        return format_html('<img src="{}" width="60" height="60">'.format(obj.i_imgage.url))
+    list_display=('i_name', 'i_price', 'i_desc', 'category', 'item_image')
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -11,3 +11,19 @@ class Student(models.Model):
     name=models.CharField(max_length=255)
     email_id=models.CharField(max_length=255)
     city=models.CharField(max_length=50)
+
+class Category(models.Model):
+    cat_name=models.CharField(max_length=200)
+    cat_desc=models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.cat_name
+
+class Item(models.Model):
+    i_name=models.CharField(max_length=255)
+    i_price=models.CharField(max_length=20)
+    i_desc=models.CharField(max_length=255)
+    i_imgage=models.ImageField(upload_to='item_image/')
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.i_name
